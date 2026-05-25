51c812615212e23bca21c8c41dec07163e8c011b
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-05-25 21:19:02 +0700

    update postgresql adapter

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,10 +1,15 @@
-import { withAccelerate } from "@prisma/extension-accelerate";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-import { PrismaClient } from "../src/generated/client";
+import { PrismaClient } from "../src/generated/prisma/client";
+import { env } from "./lib/env";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+  connectionString: env.DATABASE_URL,
+});
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-export const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export { prisma };
