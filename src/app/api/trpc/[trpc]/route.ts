c2cd65b848bc8a8af55eb29f29c0b33286ce3eea
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-06-03 08:49:39 +0700

    fix CI failed

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -7,6 +7,6 @@ const handler = (req: Request) =>
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => createTRPCContext({ headers: req.headers }),
+    createContext: () => createTRPCContext(),
   });
 export { handler as GET, handler as POST };
