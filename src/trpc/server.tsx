eb27a672abb5f17b2395bd7a42ff7e1c9d5d7617
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-06-03 14:23:00 +0700

    fix build

--- src/trpc/server.tsx
+++ src/trpc/server.tsx
@@ -1,7 +1,6 @@
 import "server-only"; // <-- ensure this file cannot be imported from the client
 
 import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
-import { headers } from "next/headers";
 import { cache } from "react";
 
 import { createTRPCContext } from "./init";
@@ -11,10 +10,7 @@ import { appRouter } from "./routers/_app";
 //            will return the same client during the same request.
 export const getQueryClient = cache(makeQueryClient);
 export const trpc = createTRPCOptionsProxy({
-  ctx: async () =>
-    createTRPCContext({
-      headers: await headers(),
-    }),
+  ctx: async () => createTRPCContext(),
   router: appRouter,
   queryClient: getQueryClient,
 });
