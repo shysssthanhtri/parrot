9be0abf66fb6c65cbadcbf1a1da25bbbd435a221
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-06-04 07:19:47 +0700

    voices page loading

---
+++ src/app/(cms)/cms/voices/loading.tsx
@@ -0,0 +1,49 @@
+import { Skeleton } from "@/components/ui/skeleton";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+const SKELETON_ROW_COUNT = 6;
+
+export default function VoicesLoading() {
+  return (
+    <div className="flex flex-col gap-4 p-4 md:p-6">
+      <div>
+        <h1 className="text-2xl font-semibold tracking-tight">Voices</h1>
+      </div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Name</TableHead>
+            <TableHead>Language</TableHead>
+            <TableHead>Description</TableHead>
+            <TableHead>Updated</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {Array.from({ length: SKELETON_ROW_COUNT }, (_, index) => (
+            <TableRow key={index}>
+              <TableCell>
+                <Skeleton className="h-4 w-32" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-16" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-48 max-w-md" />
+              </TableCell>
+              <TableCell>
+                <Skeleton className="h-4 w-24" />
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
