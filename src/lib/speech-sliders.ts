ab0814f976f40d52010d9667c06a46d18aedebea
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-06-08 09:46:23 +0700

    reverse speech config

--- src/lib/speech-sliders.ts
+++ src/lib/speech-sliders.ts
@@ -82,10 +82,10 @@ export type SpeechTtsParams = {
 };
 
 export const DEFAULT_SPEECH_TTS_PARAMS: SpeechTtsParams = {
-  temperature: 0.5,
-  topP: 0.9,
+  temperature: 0.8,
+  topP: 0.95,
   topK: 1000,
-  repetitionPenalty: 1.0,
+  repetitionPenalty: 1.2,
   normLoudness: DEFAULT_NORM_LOUDNESS,
 };
 
