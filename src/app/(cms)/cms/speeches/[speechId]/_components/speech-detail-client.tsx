de1266bfc0e2f45cc4e2b7ab0c421a1f314f165c
tringuyen-theiconic <tri.nguyen@theiconic.com.au>
2026-06-12 20:49:18 +0700

    update interval

--- src/app/(cms)/cms/speeches/[speechId]/_components/speech-detail-client.tsx
+++ src/app/(cms)/cms/speeches/[speechId]/_components/speech-detail-client.tsx
@@ -16,7 +16,7 @@ import { useTRPC } from "@/trpc/client";
 import { SpeechDeleteButton } from "./speech-delete-button";
 import { SpeechDetail } from "./speech-detail";
 
-const POLL_INTERVAL_MS = 60_000;
+const POLL_INTERVAL_MS = 10_000;
 
 type SpeechDetailClientProps = {
   speechId: string;
